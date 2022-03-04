35c22d7040373fc73adb2a0ee1b0203d28f30978
jkaram7 <jkaram@uchicago.edu>
2022-03-04 19:19:54 +0000

    messages working and starting to look pretty, v3

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,14 +11,15 @@ class MessagesController < ApplicationController
     @list_of_users = User.all
     @list_of_messages = Message.all
     
-    matching_messages = Message.where(sender_id: session.fetch(:user_id)).or(Message.where(recipient_id: session.fetch(:user_id))).order({ :created_at => :desc })
+    matching_messages = Message.where(sender_id: session.fetch(:user_id)).order({ :created_at => :desc })
+    #matching_messages = Message.where(sender_id: session.fetch(:user_id)).or(Message.where(recipient_id: session.fetch(:user_id))).order({ :created_at => :desc })
 
     if matching_messages != nil
       @list_of_threads = matching_messages.distinct.pluck(:acquisition_id, :sender_id, :recipient_id)
     else
       @list_of_threads = nil
     end
-    
+
     render({ :template => "messages/index.html.erb" })
   end
 
